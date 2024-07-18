628b4259af1e3cce91a79e2fecd3e057112ed628
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-18 10:31:14 +0200

    set update cookie front

--- client/src/pages/Connexion.jsx
+++ client/src/pages/Connexion.jsx
@@ -35,7 +35,7 @@ function Connexion() {
       login(user.data);
       setPassword("");
       setPseudo("");
-      navigate("/game"); // Redirige l'utilisateur vers la page d'accueil après une connexion réussie
+      navigate("/game"); 
     } catch (error) {
       console.error(error.message);
     }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,48 @@
-import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+import axios from "axios";
 
+import { useAuth } from "../context/AuthContext";
 import Upgrade from "../components/Upgrade";
 
 import argent from "../assets/images/argent.png";
 
+const url = import.meta.env.VITE_API_URL;
+
 function Home() {
   const { player } = useAuth();
-  console.info(player);
+  const { cookie, id, multiplicateur } = player;
+  const [cookies, setCookies] = useState(cookie);
+
+  const moreCookie = async () => {
+    setCookies(cookies + 1 * multiplicateur);
+
+    try {
+      await axios.put(
+        `${url}/api/player/${player.id}`,
+        { cookie: cookies, id },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <h2>{player.pseudo}: <img src={argent} alt="un cookie en guise de money" className="argent"/><em>{player.cookie}</em></h2>
-      <img src={argent} alt="le cookie a clicker" className="cookie"/>{" "}
+      <h2>
+        {player.pseudo}:{" "}
+        <img
+          src={argent}
+          alt="un cookie en guise de money"
+          className="argent"
+        />
+        <em style={{paddingLeft: ".5rem"}}>{cookies}</em>
+      </h2>
+      <button type="button" className="moreCookie" onClick={moreCookie}>
+        <img src={argent} alt="le cookie a clicker" className="cookie" />
+      </button>{" "}
       <Upgrade />
     </>
   );
