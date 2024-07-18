87fe4c0c0e41b29de311a8d9173199aae7b79aeb
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-18 18:28:49 +0200

    style

--- client/src/pages/Acceuil.jsx
+++ client/src/pages/Acceuil.jsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
+
 function Acceuil() {
-  return <>Ici est le composant d'acceuil</>;
+  return (
+    <>
+      <h1>Bienvenue</h1>
+      <section>
+        <article>
+          <h2>Comment jouer?</h2>
+          <p>
+            Pour jouer c'est tres simple, il suffit de se creer un compte et de
+            se connecter. Une fois connecter, plus qu'a appuyer sur le cookie
+            geant en plein milieu de la page ! A chaque click, votre nombre de
+            cookie augmentera. Au debut seulement un part un, pour avoir plus de
+            cookies par click, il y a differentes amelioration qui augmente le
+            nombre de cookies.
+          </p>
+
+          
+        </article>
+        <article className="blockBouton">
+        <Link to="/connexion" className="pasDeco">
+            <button type="button" className="upgradeButton">
+              Connexion{" "}
+            </button>
+          </Link>
+          <Link to="/inscription" className="pasDeco">
+            <button type="button" className="upgradeButton">
+              Inscription
+            </button>
+          </Link>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default Acceuil;
